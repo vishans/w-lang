3a02e36901af95f04af2a05d2d0dd91d929debf1
Vishan Summinga Sonagadu <94801273+vishans@users.noreply.github.com>
2022-05-01 03:50:37 +0400

    Fix responsiveness issues

--- script/doc_button.js
+++ script/doc_button.js
@@ -1,10 +1,12 @@
 sideBarButton = $('#side-bar-button');
 scrollTopButton = $('#top-of-page-button');
 sideBar = $('#side-bar');
+
 if($(window).innerWidth() < 769){
-    console.log('hehe')
 sideBar.removeClass('side-bar');
 sideBar.addClass('mobile-side-bar');}
+
+
 let t = ($(window).innerHeight() - sideBar.innerHeight())/2;
 let l = ($(window).innerWidth() - sideBar.innerWidth())/2;
 sideBar.css({'top':t + 'px','left':l +'px'});
@@ -19,11 +21,12 @@ sideBarButton.on('click', function(){
 
         sideBarButton.text('close');
         sideBar.fadeIn();
+        sideBar.css("display","absolute");
         sideButtonActive = true;
         return
     }
 
-    sideBar.fadeOut();
+        sideBar.fadeOut();
         $('body').css({'overflow-y' : ' scroll'})
         $('.text-box').css({'color': 'black',
             'text-shadow': 'none'})
@@ -39,3 +42,56 @@ sideBarButton.on('click', function(){
 scrollTopButton.on('click', function(){
     window.scrollTo(0,0);
 })
+
+$('.text-box').on('click', function (){
+    if(sideButtonActive){
+        sideBar.fadeOut();
+        $('body').css({'overflow-y' : ' scroll'})
+        $('.text-box').css({'color': 'black',
+            'text-shadow': 'none'})
+
+
+        sideBarButton.text('list');
+
+        sideButtonActive = false;
+        
+    }
+})
+
+
+
+$(window).resize(function(){
+    if($(window).innerWidth() < 769){
+
+        sideBar.removeClass('side-bar');
+        sideBar.addClass('mobile-side-bar');
+        $('#side-bar').css({'display': 'none'})
+
+        if(sideButtonActive){
+            sideBar.fadeOut();
+            $('body').css({'overflow-y' : ' scroll'})
+            $('.text-box').css({'color': 'black',
+                'text-shadow': 'none'})
+    
+    
+            sideBarButton.text('list');
+    
+            sideButtonActive = false;
+        }
+
+        
+
+    }
+    else{
+        console.log('heee')
+        sideBar.removeClass('mobile-side-bar');
+        sideBar.addClass('side-bar');
+        $('#side-bar').css({'display': 'block'});
+        $('.text-box').css({'color': 'black',
+        'text-shadow': 'none'});
+
+       
+    }
+})
+
+
